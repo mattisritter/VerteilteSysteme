ed1f401df6b1cb2f13b1e145b7ed3f87f99140a8
mattisritter <mattis.tom.ritter@gmail.com>
2022-10-24 11:32:36 +0200

    alles mögliche

---
+++ Code/Heizkoerperthermostat/Servo.c
@@ -0,0 +1,7 @@
+/*
+ * Servo.c
+ *
+ * Created: 24.10.2022 11:17:59
+ *  Author: matti
+ */ 
+
---
+++ Code/Heizkoerperthermostat/Servo.h
@@ -0,0 +1,16 @@
+/*
+ * Servo.h
+ *
+ * Created: 24.10.2022 11:17:37
+ *  Author: matti
+ */ 
+
+
+#ifndef SERVO_H_
+#define SERVO_H_
+
+
+
+
+
+#endif /* SERVO_H_ */
