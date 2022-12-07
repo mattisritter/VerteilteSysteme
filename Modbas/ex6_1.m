b5436816b84179813b3060fc45d00ac6ec2ffba1
mattisritter <mattis.tom.ritter@gmail.com>
2022-12-07 15:53:55 +0100

    modbas

--- Modbas/ex6_1.m
+++ Modbas/ex6_1.m
@@ -18,7 +18,8 @@ kr = Ti/k * sqrt(omegaD^2 + omegaD^4 * T^2) / sqrt(Ti^2*omegaD^2 + 1);
                         % Verstärung des Geschwindigkeitsreglers [s/m]
 % Positionsreglerparameter                        
 kp = 1;                 % Verstärkung des Positionsregler [1/s]  
-                        
+% Abtastung
+tA = 20e-3;
 %% Übertragungsfunktionen
 s = tf('s');
 % Geschwindigkeitsregler
@@ -47,8 +48,9 @@ clf;
 step(G_W), grid on;   
 % Analyse der Sprungantwort
 si = stepinfo(G_W);
-Tm = si.PeakTime;       % Überschwingzeit [s]
-em = si.Overshoot;      % Überschwingweite [%]
+Tm = si.PeakTime       % Überschwingzeit [s]
+em = si.Overshoot      % Überschwingweite [%]
+
 %% Rampenantwort Positionsregler
 v_s = 0.1;              % Soll Geschwindigkeit [m/s]
 tsim = 0:0.01:10;       % Zeitintervall für die Rampe
@@ -61,11 +63,13 @@ lsim(G_Wp, u, tsim); grid on;
 legend('y(t)');
 
 %% Stabilität
+figure(4);
+clf;
+rlocus(G_Wp);
 [r,gain] = rlocus(G_Wp);
 for i = 1:length(gain)
     if real(r(2,i)) > 0 
-       disp('kpmax = ');
-       disp(gain(i-1));
+       kpmax = gain(i-1)
        break;
     end 
 end
@@ -81,7 +85,7 @@ xs = 1;     % Soll-Bogenlänge x* [m]
 % Berechnung der Koeffizienten cff für die Online-Vorsteuerung uvp1
 cff = cd_refpoly_ff(c, k, T, Tt, kr, Ti);
 
-t = 0:0.05:te; % Definiton des Zeitintervalls
+t = 0:tA:te; % Definiton des Zeitintervalls
 
 % Bestimmung der Polynome
 uvp1 = polyval(cff, t);
@@ -92,19 +96,30 @@ cdd = polyder(cd);  % Koeffizienten von wpdd
 wpdd = polyval(cdd, t);
 
 % Plot für Signal uvp1 für die Online-Vorsteuerung
-figure(4);
+figure(5);
 clf;
 plot(t,uvp1); grid on;
 legend('uvp1(t)');
 % Plot für das Führungssignal wp und die Ableitungen wpd und wpdd
-figure(5);
+figure(6);
 clf;
 plot(t,wp, t,wpd, t,wpdd); grid on; 
 legend('wp(t)', 'wpd(t)', 'wpdd(t)');
 
 % Plot des Position y im Vergleich zum Führungsignal
-figure(6);
+figure(7);
 clf;
 lsim(G_Wp, wp, t); grid on;
 legend('y(t)');
 
+%% TEST 
+t2 = 0:tA:2*te;
+G_Vp1 = s/(Ti*s+1);
+uvp12 = [uvp1, ones(size(uvp1)).*xs];
+wp2 = [wp, ones(size(wp)).*xs];
+uvp = transpose(lsim(G_Vp1, uvp12, t2));
+y = transpose(lsim(G_Wp, wp2+uvp, t2));
+figure(8); clf;
+plot(t2,y, t2,wp2); grid on;
+figure(9); clf;
+lsim(G_Vp1, uvp12, t2); grid on;
